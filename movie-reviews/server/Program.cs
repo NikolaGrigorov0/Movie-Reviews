5c41e5a32acd7797e1fc92087fe70c935dae1eff
NikolaGrigorov0 <ngrigorov135@gmail.com>
2025-02-10 16:15:31 +0200

    movie review no longer required

--- movie-reviews/server/Program.cs
+++ movie-reviews/server/Program.cs
@@ -72,7 +72,3 @@ if (app.Environment.IsDevelopment())
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- movie-reviews/server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ movie-reviews/server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3722f3d2c61309095a1f1a7f87bd31998d71756c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e791c49fd5b28a390bceb3c7012f626101e9244")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
