b3e28723222aa7a9a9d23f7ec7ae5167ee2c93c1
NikolaGrigorov0 <ngrigorov135@gmail.com>
2025-02-13 20:19:35 +0200

    static data folder added

--- movie-reviews/server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ movie-reviews/server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0adae85fb886ffcc74b84a7d04463a609a5361d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28202ded0282799048888135207060cb05ed1a37")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
